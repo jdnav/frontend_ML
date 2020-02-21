1dd9600582ce54a335948784a3bf5de75f4b8aa0
jdnav <josedaniel.navarro@edu.uah.es>
2020-02-22 00:12:43 +0100

    Basic credit card form

--- src/app/pages/credit-card/credit-card.component.ts
+++ src/app/pages/credit-card/credit-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-credit-card',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./credit-card.component.css']
 })
 export class CreditCardComponent implements OnInit {
+
+  creditCardForm = new FormGroup ({
+    firstName: new FormControl(),
+    lastName: new FormControl(),
+
+  });
 
   constructor() { }
 
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactComponent } from './contact/contact.component';
 import { CreditCardComponent } from './credit-card/credit-card.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import {
+  MatInputModule,
+} from '@angular/material/input';
 
 
 @NgModule({
   declarations: [ContactComponent, CreditCardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    MatInputModule
   ],
   exports: [ContactComponent, CreditCardComponent]
 })
