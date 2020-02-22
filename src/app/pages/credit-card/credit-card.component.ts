d6a05a2d7fd9386ce29858f086e4c9a1c04a3ca0
jdnav <josedaniel.navarro@edu.uah.es>
2020-02-22 15:25:16 +0100

    Basic card + form in credit card component

--- src/app/pages/credit-card/credit-card.component.ts
+++ src/app/pages/credit-card/credit-card.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
+interface SelectOption {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-credit-card',
   templateUrl: './credit-card.component.html',
@@ -8,11 +13,51 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class CreditCardComponent implements OnInit {
 
-  creditCardForm = new FormGroup ({
+  creditCardForm = new FormGroup({
     firstName: new FormControl(),
     lastName: new FormControl(),
-
+    NAME_INCOME_TYPE: new FormControl(),
+    CODE_GENDER: new FormControl(),
+    NAME_EDUCATION_TYPE: new FormControl(),
+    NAME_FAMILY_STATUS: new FormControl(),
+    NAME_HOUSING_TYPE: new FormControl(),
   });
+
+  educationTypes: SelectOption[] = [
+    { value: 'Academic degree', viewValue: 'Academic degree' },
+    { value: 'Higher education', viewValue: 'Higher education' },
+    { value: 'Incomplete higher', viewValue: 'Incomplete higher' },
+    { value: 'Lower secondary', viewValue: 'Lower secondary' },
+    { value: 'Secondary / secondary special', viewValue: 'Secondary / secondary special' },
+  ];
+
+  incomeTypes: SelectOption[] = [
+    { value: 'Commercial associate', viewValue: 'Commercial associate' },
+    { value: 'Pensioner', viewValue: 'Pensioner' },
+    { value: 'State servant', viewValue: 'state servant' },
+    { value: 'Student', viewValue: 'Student' },
+    { value: 'Working', viewValue: 'Working' },
+  ];
+
+  genderTypes: SelectOption[] = [
+    { value: 'M', viewValue: 'Male' },
+    { value: 'F', viewValue: 'Female' },
+  ];
+
+  familyStatusTypes: SelectOption[] = [
+    { value: 'Civil marriage, Married', viewValue: 'Civil marriage, Married' },
+    { value: 'Separated', viewValue: 'Separated' },
+    { value: 'Single / not married', viewValue: 'Single / not married' },
+    { value: 'Widow', viewValue: 'Widow' },
+  ];
+
+  houseTypes: SelectOption[] = [
+    { value: 'Co-op apartment', viewValue: 'Co-op apartment' },
+    { value: 'House / apartment', viewValue: 'House / apartment' },
+    { value: 'Office apartment', viewValue: 'Office apartment' },
+    { value: 'Rented apartment', viewValue: 'Rented apartment' },
+    { value: 'With parent', viewValue: 'With parent' },
+  ];
 
   constructor() { }
 
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ContactComponent } from './contact/contact.component';
 import { CreditCardComponent } from './credit-card/credit-card.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import {
-  MatInputModule,
-} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 
 @NgModule({
@@ -13,7 +14,10 @@ import {
   imports: [
     CommonModule,
     ReactiveFormsModule,
-    MatInputModule
+    MatInputModule,
+    MatSelectModule,
+    MatCardModule,
+    MatButtonModule
   ],
   exports: [ContactComponent, CreditCardComponent]
 })
