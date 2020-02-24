3f8a8fe5f239edc202557c69f2d767c287829972
jdnav <josedaniel.navarro@edu.uah.es>
2020-02-24 21:18:29 +0100

    New controls added in credit-card form

--- src/app/pages/credit-card/credit-card.component.ts
+++ src/app/pages/credit-card/credit-card.component.ts
@@ -14,14 +14,37 @@ interface SelectOption {
 export class CreditCardComponent implements OnInit {
 
   creditCardForm = new FormGroup({
-    firstName: new FormControl(),
-    lastName: new FormControl(),
+    STATUS: new FormControl(), // Finantial status
+    NAME_EDUCATION_TYPE: new FormControl(),
     NAME_INCOME_TYPE: new FormControl(),
     CODE_GENDER: new FormControl(),
-    NAME_EDUCATION_TYPE: new FormControl(),
     NAME_FAMILY_STATUS: new FormControl(),
     NAME_HOUSING_TYPE: new FormControl(),
+    CNT_CHILDREN: new FormControl(),
+    FLAG_OWN_CAR: new FormControl(),
+    FLAG_MOBIL: new FormControl(), // Mobile phone
+    FLAG_WORK_PHONE: new FormControl(), // Work phone
+    FLAG_PHONE: new FormControl(), // Landline
+    FLAG_EMAIL: new FormControl(),
+    CTM_FAM_MEMBERS: new FormControl(), // Family size
+    FLAG_OWN_REALITY: new FormControl(),
+    FLAG_BIRTH: new FormControl(),
+    DAYS_EMPLOYED: new FormControl(),
+    MONTH_BALANCE: new FormControl(),
+    AMT_INCOME_TOTAL: new FormControl(),
+    OCCUPATION_TYPE: new FormControl(),
   });
+
+  finantialStatus: SelectOption[] = [
+    { value: '0', viewValue: '1-29 days past due' },
+    { value: '1', viewValue: '30-59 days past due' },
+    { value: '2', viewValue: '60-89 days overdue' },
+    { value: '3', viewValue: '90-119 days overdue' },
+    { value: '4', viewValue: '120-149 days overdue' },
+    { value: '5', viewValue: 'write-offs for more than 150 days' },
+    { value: 'C', viewValue: 'paid off that month' },
+    { value: 'X', viewValue: 'No loan for the month' }
+  ];
 
   educationTypes: SelectOption[] = [
     { value: 'Academic degree', viewValue: 'Academic degree' },
